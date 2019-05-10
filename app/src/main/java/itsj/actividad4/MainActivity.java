7a6134f420cc1808c8a3651dc1bdb70586089493
Benjamin Viramontes <benja120599@gmail.com>
2019-05-10 14:10:58 -0500

    Se instanciaron las variables de instancia

--- app/src/main/java/itsj/actividad4/MainActivity.java
+++ app/src/main/java/itsj/actividad4/MainActivity.java
@@ -13,4 +13,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText cajaEntrada, cajaC, cajaF, cajaR, cajaK;
     CheckBox opcionC, opcionF, opcionR, opcionK;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gradosC = findViewById(R.id.radioButtonC);
+        gradosC.setOnClickListener(this);
+        gradosF = findViewById(R.id.radioButtonF);
+        gradosF.setOnClickListener(this);
+        gradosK = findViewById(R.id.radioButtonK);
+        gradosK.setOnClickListener(this);
+        gradosR = findViewById(R.id.radioButtonR);
+        gradosR.setOnClickListener(this);
+
+        cajaEntrada = findViewById(R.id.cajaEntrada);
+        cajaEntrada.setOnClickListener(this);
+        cajaC = findViewById(R.id.cajaC);
+        cajaC.setOnClickListener(this);
+        cajaF = findViewById(R.id.cajaF);
+        cajaF.setOnClickListener(this);
+        cajaK = findViewById(R.id.cajaK);
+        cajaK.setOnClickListener(this);
+        cajaR = findViewById(R.id.cajaR);
+        cajaR.setOnClickListener(this);
+
+        opcionC = findViewById(R.id.checkBoxC);
+        opcionC.setOnClickListener(this);
+        opcionF = findViewById(R.id.checkBoxF);
+        opcionF.setOnClickListener(this);
+        opcionK = findViewById(R.id.checkBoxK);
+        opcionK.setOnClickListener(this);
+        opcionR = findViewById(R.id.checkBoxR);
+        opcionR.setOnClickListener(this);
+
+    }
+
 }
