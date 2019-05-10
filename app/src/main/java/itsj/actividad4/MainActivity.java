a7d14ae730752dfc2ea5d8fe53ade8c5aaba9e7b
Benjamin Viramontes <benja120599@gmail.com>
2019-05-10 14:04:02 -0500

    Se añadió el proyecto al repositorio

---
+++ app/src/main/java/itsj/actividad4/MainActivity.java
@@ -0,0 +1,13 @@
+package itsj.actividad4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+}
